570170764adcd162bc27c7c75b2a18f33431eddf
Nick <deckards2505@gmail.com>
2020-08-22 11:59:05 +0600

    Init repo

---
+++ dist/js/main.js
@@ -0,0 +1,6 @@
+$(function () {
+    
+    // $('.menu__btn, .menu__close').on('click', function () {
+    //     $('.menu__navigation, .menu__close').toggleClass('menu__navigation--active');
+    // });
+});
